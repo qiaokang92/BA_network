30b3fe9395650626c6e5d29576e73e688e11f15a
buaakq <kkqq666666@163.com>
2014-10-30 12:08:39 +0800

    finish writing options

--- static_struct.py
+++ static_struct.py
@@ -1,8 +1,23 @@
+'''
+This file includes some functions for 2 graphs.
+The first graph G is a graph of banks, the second one includes the strike between banks.
+ 
+Author: Kang Qiao, BUAA
+Finsh Date: Oct, 25th, 2014
+'''
+import random
 import networkx as nx
+
+'''
+build a BA network, with n nodes and each step connet m nodes
+this network is directed and have multiple edges
+modified from barabasi_albert_graph() in networkx.random_graphs module
+'''
 
 def build_network(n,m):
     return nx.random_graphs.my_BA_graph(n,m)
 
+# return attributes of every edges in G
 def get_edges_attr(G):
     result = []
     for i in G.edges():
@@ -12,13 +27,15 @@ def get_edges_attr(G):
       show['edge'] = i
       result.append(show)
     return result
-      
+
+# return attributes of edge in G
 def get_edge_attr(G,edge):
     start = edge[0]
     end = edge[1]
     attr = G[start][end][0]
     return attr
-      
+
+# return attirbutes of every nodes in G
 def get_nodes_attr(G):
     result = []
     for i in G.nodes():
@@ -27,6 +44,7 @@ def get_nodes_attr(G):
       result.append(show)
     return result
 
+# init the 'e' value of all edges in G 
 def init_edges_attr_e(G):
     for i in G.edges():
       first = i[0]
@@ -37,7 +55,8 @@ def init_edges_attr_e(G):
         #print i
         #print first_in
         #print G.edge[first][to][0]   
-    
+
+# init the 'L' and 'B' value of all nodes in G
 def init_nodes_LB(G):
     for i in G.nodes():
       result = 0
@@ -47,26 +66,27 @@ def init_nodes_LB(G):
       G.node[i]['L'] = 0.2
       G.node[i]['B'] = result
 
-#receive 2 lists with n elements, like [0.6, 0.7, ...]
+#  init the 'c' and 'S' value of all nodes in G
+#  receive 2 lists, each has n elements, like [0.6, 0.7, ...]
 def init_nodes_CS(G,c,s):
     for i in G.nodes():
       G.node[i]['c'] = c[i]
       G.node[i]['S'] = s[i]
 
-def init_nodes_S(G,m_list):
-    for i in m_list:
-      G.node[i]['S'] = 1
-
+# init the 'statue' value as 'Stable' of all nodes in G
 def init_nodes_status(G):
     for i in G.nodes():
       G.node[i]['status'] = 'Stable'
 
+# init the 'shock' value as zero of all edges in ST
 def init_impact_between_nodes(ST):
     for i in ST.edges():
       start = i[0]
       to = i[1]
       ST[start][to][0]['shock'] = 0
 
+# update the status and the 'c' value of every node in G
+# by the change of 'S' value
 def update_nodes_status(G):
     for i in G.nodes():
       if G.node[i]['c'] > G.node[i]['S']:
@@ -76,6 +96,8 @@ def update_nodes_status(G):
         G.node[i]['c'] = 0
         G.node[i]['status'] = 'Default'
 
+# update the impact from i node to j node in G
+# by the change of status and 'c' value
 def update_impact_between_nodes(G,ST):
     for i in ST.edges():
       first = i[0]
@@ -94,7 +116,9 @@ def update_impact_between_nodes(G,ST):
         ST.edge[first][end][0]['shock'] = last_E
       else:
         ST.edge[first][end][0]['shock'] = (last_S - last_c) * last_E / L
-    
+  
+# calculate the 'S' value of all nodes in G
+# by the change of 's' value of edges in ST  
 def udpate_nodes_S(G,ST):
     n = len(G)
     for i in G.nodes():
@@ -103,10 +127,13 @@ def udpate_nodes_S(G,ST):
         if i != j:
           G.node[i]['S'] += ST.edge[j][i][0]['shock']
 
+# set the 'S' value of node in G as 1
+# from the list 'm', which contains a list of banks
 def set_nodes_S(G,m):
     for i in m:
       G.node[i]['S'] = 1
 
+# after loops, calculate the number of default banks.
 def get_default_num(G):
     counter = 0
     for i in G.nodes():
@@ -114,10 +141,27 @@ def get_default_num(G):
         counter += 1
     return counter
 
+# package function, to build a directed BA graph,
+# and initialize the 'c', 'S', 'L', 'B', 'status' value of all nodes in G
+# and initialize the 'e' value of all edges in G 
+def init_myBA(n,n0,c_list,S_init_list):
+    myBA = build_network(n,n0)
+    init_edges_attr_e(myBA)
+    init_nodes_LB(myBA)
+    init_nodes_CS(myBA,c_list,S_init_list) 
+    init_nodes_status(myBA)
+    return myBA
 
+# package function, to build a complete directed graph 
+# and initialize the 'shock' value of all edges in ST
+def init_ST(n):
+    ST = nx.complete_graph(n, create_using = nx.MultiDiGraph())
+    init_impact_between_nodes(ST)
+    return ST
 
-
-
-
+def get_random_list(n,m):
+    n_list = range(0,n)
+    m_list =  random.sample(n_list,m)
+    return m_list
 
 
--- test.py
+++ test.py
@@ -1,61 +1,117 @@
+
 import networkx as nx
+import sys
+import os 
+import signal
+import time
 import matplotlib.pyplot as plt
+from optparse import OptionParser
 from static_struct import *
+#from static_vars import *
 
-# number of banks
-n = 20
 
-# second parameter of building BA network
-n0 = 1
+def signal_handler(signal, frame):
+    print '\nProgram Exit'
+    sys.exit(0)
 
-# number of nodes chosen to give a shock
-m = 3
-
-# or given banks instead of random bannks
-m_list = [1,2,3,4,5,6,7]
-
-# banks' property
-c_list = [0.6] * n
-
-# to initialize the bank's impact from others
-S_init_list = [0] * n
-
-# maximum times
-max_loop_time = range(10)
-
-def main():
-    myBA = build_network(n,n0)
+def parse_para():
+    signal.signal(signal.SIGINT, signal_handler)
+   
+    parser = OptionParser(usage = '\n    python %prog -n number_of_banks --manual [options]\n    python %prog -n number_of_banks -m bank_chosen_num [options]', 
+                          description = 'this algorithm aimed ......')
     
-    init_edges_attr_e(myBA)
-    init_nodes_LB(myBA)
-    init_nodes_CS(myBA,c_list,S_init_list)
-    init_nodes_status(myBA) 
-     
-    print myBA.edges() 
-    print get_edges_attr(myBA)
-    print get_nodes_attr(myBA)
+    parser.add_option("-n", action = 'store',
+                            dest = "BANK_NUM", 
+                            help = "The number of banks")
+
+    parser.add_option("-i", dest = "N0",
+                            type = "int", 
+                            default = 1,
+                            action = 'store',
+               help = "nodes connected every step, default is 1")
     
-    ST = nx.complete_graph(n, create_using = nx.MultiDiGraph())
-    init_impact_between_nodes(ST)   
+    parser.add_option("-m", dest = 'BANK_CHOSEN_NUM',
+                            action = 'store',
+          help = 'Once this option is given, m banks will be give a shock as 1')
     
-    print get_edges_attr(ST)
+    parser.add_option( "--manual", dest = 'verbose',
+                                 action = 'store_true',
+                                 default = False,
+                        help = 'Choose banks manually')
     
-    for time in max_time:
+    parser.add_option( "--lamuta", dest = "LAMUTA",
+                               action = 'store',
+                               default = 0,
+                         help = 'Banks\' initial property')
+
+    (options, args) = parser.parse_args()
+    
+    if not options.BANK_NUM:
+      print >> sys.stderr, 'ERROR : total number is not given!\n'
+      parser.print_help()
+      sys.exit(1)
+    else:
+      n = int(options.BANK_NUM)
+
+    if not options.N0:
+      print >> sys.stderr, 'WARNING : step number is not given, set as 1'      
+      n0 = 1
+ 
+    if options.N0 > n:
+      print >> sys.stderr, 'WARNING : step number too large, set as 1'
+      n0 = 1   
+  
+    if not options.LAMUTA:
+      print >> sys.stderr, 'WARNING : lamuta is not given, set as 0'
+
+    if options.BANK_CHOSEN_NUM:
+      m = int(options.BANK_CHOSEN_NUM)
+      m_list = get_random_list(n,m)
+    elif options.verbose:
+      loop = True
+      while(loop):
+        m_try = raw_input('Please in put your numbers, seperate by comma:')
+        m_list = eval(m_try) 
+        if type(m_list) is int:
+          tmp = m_list
+          m_list = []
+          m_list = [tmp]
+        loop = False
+        for i in m_list:
+          if i > n :
+            print >> sys.stderr, 'One or more given numbers is too large!'
+            loop = True
+        if len(m_list)  > n:
+            print >> sys.stderr, 'number of numbers is larger than population!'
+            loop = True
+    else:
+      parser.print_help()
+      sys.exit(1)
+
+    lamuta = int(options.LAMUTA)
+    c_list  = n * [lamuta]
+    S_init_list = n * [0]
+    max_loop_times = range(20)
+
+    print n
+    print len(m_list)
+    print m_list
+    main(n, n0, m_list, c_list, S_init_list, max_loop_times)
+    
+
+def main(n, n0, m_list, c_list, S_init_list, max_loop_times):
+    myBA = init_myBA(n,n0,c_list,S_init_list)
+    ST = init_ST(n) 
+    
+    for time in max_loop_times:
       if time == 0:
         set_nodes_S(myBA,m_list)
       update_nodes_status(myBA)
       update_impact_between_nodes(myBA,ST)
       udpate_nodes_S(myBA,ST)
     
-    print get_nodes_attr(myBA)       
-    
     default_num = get_default_num(myBA)
-
     print default_num      
 
-
-
-
-
 if __name__ == '__main__':
-    main()
+    parse_para()
